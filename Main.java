80a3c10d426ea65963a854fa076b5f9e5e3ae494
MUTUA JAMES MUTHIANI <99032516+james05-cpu@users.noreply.github.com>
2022-02-12 13:50:49 +0300

    uploads

--- Main.java
+++ Main.java
@@ -1,59 +1,51 @@
-    package com.Annonymous;
-        
-        import javax.swing.*;
-        import java.awt.event.ActionEvent;
-        import java.awt.event.ActionListener;
-        /* window where user can choose game level
-        to play from a menu.... will launch game here
-         */
-        public class Main {
-            public static void main(String[]args){
-                JFrame obj= new JFrame();
-        JMenuBar menuBar=new JMenuBar();
-        JMenu level=new JMenu("Level");
-        JMenuItem normal=new JMenuItem("Normal");
-                JMenuItem about=new JMenuItem("About");
-                about.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        JOptionPane.showMessageDialog(null,"use left and right to \n" +
-                                "to control the ball");
-                    }
-                });
-        
-        level.add(normal);
-                JMenuItem hard=new JMenuItem("Expert");
-                level.add(hard);
-                level.add(about);
-                menuBar.add(level);
-        obj.setJMenuBar(menuBar);
+package com.Annonymous;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[]args){
+        JFrame obj= new JFrame();
         Gameplay gameplay=new Gameplay();
-        hard.addActionListener(new ActionListener() {
+
+/*JMenuBar menuBar=new JMenuBar();
+JMenu level=new JMenu("Level");
+JMenuItem normal=new JMenuItem("Normal");
+level.add(normal);
+        JMenuItem hard=new JMenuItem("Expert");
+        level.add(hard);
+        menuBar.add(level);
+obj.setJMenuBar(menuBar);
+hard.addActionListener(new ActionListener() {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        gameplay.playerlen=25;
+        gameplay.leftWall=675;
+        gameplay.rightWall=6;
+        gameplay.ballydir=-4;
+        gameplay.ballxdir=-5;
+    }
+});
+       normal.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                gameplay.playerlen=25;
-                gameplay.leftWall=675;
-                gameplay.rightWall=6;
-                gameplay.ballydir=-4;
-                gameplay.ballxdir=-5;
+                gameplay.playerlen=100;
+                gameplay.leftWall=600;
+                gameplay.rightWall=10;
+                gameplay.ballydir=-1;
+                gameplay.ballxdir=-2;
             }
         });
-               normal.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        gameplay.playerlen=100;
-                        gameplay.leftWall=600;
-                        gameplay.rightWall=10;
-                        gameplay.ballydir=-1;
-                        gameplay.ballxdir=-2;
-                    }
-                });
-               obj.add(gameplay);
-        
-             obj.setBounds(10,10,700,633);
-                obj.setFocusable(false);
-                obj.setResizable(false);
-                obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                obj.setVisible(true);
-            }
-        }
+
+
+ */        obj.add(gameplay);
+
+        obj.setBounds(10,10,700,633);
+        //obj.setLocationRelativeTo(null);
+        obj.setFocusable(false);
+        obj.setResizable(false);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.setVisible(true);
+    }
+}
--- MapGenerator.java
+++ MapGenerator.java
@@ -1,38 +1,36 @@
- package com.Annonymous;
-        
-        import java.awt.*;
-        /*create a game map that will use to draw the bricks on
-        game window
-         */
-        public class MapGenerator {
-            int[][]map;
-            int brickwidth;
-            int brickheight;
-        
-            public MapGenerator(int row, int col) {
-              map=new int[row][col];
-              for (int i=0; i<map.length; i++){
-                  for (int j=0; j<map[0].length; j++){
-                      map[i][j]=1;
-                  }
-              }
-              brickwidth=540/col;
-              brickheight=150/row;
-            }
-            public void draw(Graphics2D g){
-                for (int i=0; i<map.length; i++){
-                    for (int j=0; j<map[0].length; j++){
-                        if (map[i][j]>0){
-                            g.setColor(Color.white);
-                            g.fillRect(j*brickwidth+80,i*brickheight+50,brickwidth,brickheight);
-                       g.setStroke(new BasicStroke(3));
-                        g.setColor(Color.BLACK);
-                        g.drawRect(j*brickwidth+80,i*brickheight+50,brickwidth,brickheight);
-                        }
-                    }
+package com.Annonymous;
+
+import java.awt.*;
+
+public class MapGenerator {
+   int[][]map;
+   int brickwidth;
+    int brickheight;
+
+    public MapGenerator(int row, int col) {
+      map=new int[row][col];
+      for (int i=0; i<map.length; i++){
+          for (int j=0; j<map[0].length; j++){
+              map[i][j]=1;
+          }
+      }
+      brickwidth=540/col;
+      brickheight=150/row;
+    }
+    public void draw(Graphics2D g){
+        for (int i=0; i<map.length; i++){
+            for (int j=0; j<map[0].length; j++){
+                if (map[i][j]>0){
+                    g.setColor(Color.white);
+                    g.fillRect(j*brickwidth+80,i*brickheight+50,brickwidth,brickheight);
+               g.setStroke(new BasicStroke(3));
+                g.setColor(Color.BLACK);
+                g.drawRect(j*brickwidth+80,i*brickheight+50,brickwidth,brickheight);
                 }
             }
-            public  void  setBrickValue(int value,int row ,int col){
-             map[row][col]=value;
-            }
         }
+    }
+    public  void  setBrickValue(int value,int row ,int col){
+     map[row][col]=value;
+    }
+}
